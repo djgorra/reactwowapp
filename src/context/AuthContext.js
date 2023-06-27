222562291b3b732438aea88d0cc0fe8dd6909186
Don Gorra <djgorra93@gmail.com>
2023-06-27 15:09:15 -0400

    Logout function and error boxes for login/register

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,12 +2,18 @@ import axios from "axios";
 import React, {createContext, useState} from "react";
 import { BASE_URL } from "../config";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert} from 'react-native';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const [userInfo, setUserInfo] = useState({});
     const [isLoading, setIsLoading] = useState(false)
+
+    const createButtonAlert = (e) =>
+    Alert.alert('Alert', `${e}`, [
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
 
     const register = (name, email, password) => {
         setIsLoading(true);
@@ -20,13 +26,30 @@ export const AuthProvider = ({children}) => {
             setIsLoading(false);
             console.log(userInfo);
         })
-        .catch(e => {
-            console.log(`register error ${e}`);
-            if(e.response){
-                console.log(e.response.data)
+        .catch((error) => {
+            // Error
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                createButtonAlert(error.response.data.message)
+                // console.log(error.response.status);
+                // console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the 
+                // browser and an instance of
+                // http.ClientRequest in node.js
+                createButtonAlert("Network Error")
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                createButtonAlert("An error has occurred :(")
+                console.log('Error', error.message);
             }
-            setIsLoading(false);
-        });
+            setIsLoading(false)
+            console.log(error.config);
+        })
     };
 
     const login = (email, password) => {
@@ -38,29 +61,46 @@ export const AuthProvider = ({children}) => {
             AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
             setIsLoading(false);
             console.log(userInfo);
-        }).catch(e => {
-            console.log(e)
-            if(e.response){
-                console.log(`login error ${e.response.data}`)
+        }).catch((error) => {
+            // Error
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                createButtonAlert("Invalid Email or Password")
+                // console.log(error.response.status);
+                // console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the 
+                // browser and an instance of
+                // http.ClientRequest in node.js
+                createButtonAlert("Network Error")
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                createButtonAlert("An error has occurred :(")
+                console.log('Error', error.message);
             }
-            setIsLoading(false);
-        });
+            setIsLoading(false)
+            console.log(error.config);
+        })
     }
 
     const logout = () => {
         setIsLoading(true);
-        axios.post(`${BASE_URL}/api/users/sign_out`, {}, {
-            headers: {Authorization: `Bearer ${userInfo.access_token}`}
-            }
-        ).then(res => {
-            console.log(res.data)
+        // axios.delete(`${BASE_URL}/api/users/sign_out`, {}, {
+        //     headers: {HTTP_AUTHORIZATION: `Bearer ${userInfo.access_token}`}
+        //     }
+        // ).then(res => {
+        //     console.log(res.data)
             AsyncStorage.removeItem('userInfo');
             setUserInfo({})
             setIsLoading(false)
-        }).catch(e=> {
-            console.log(`logout error ${e}`);
-            setIsLoading(false);
-        });
+        // }).catch(e=> {
+        //     console.log(`logout error ${e}`);
+        //     setIsLoading(false);
+        // });
     }
 
     return (
