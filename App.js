a866524f924110b25c560a584a500993c9358ed2
Nicole Beguesse <nicole@monroneylabels.com>
2023-07-24 16:29:15 -0400

    Remove font errors

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Text, View } from "react-native";
 import Navigation from "./src/components/Navigation";
 import { AuthProvider } from "./src/context/AuthContext";
+import {DataProvider} from './src/hooks';
 
 const App = () => {
     return (
+      <DataProvider>
       <AuthProvider>
         <Navigation />
       </AuthProvider>
+      </DataProvider>
     );
 };
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,61 @@
-import React, {useContext, useState} from "react"; 
+import React, {useContext, useState, useEffect} from "react"; 
 import { Text, View, Button, Settings } from "react-native";
 
-import { NavigationContainer } from "@react-navigation/native";
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import { AuthContext } from "../context/AuthContext";
 import MainMenu from "../components/MainMenu";
+import {useFonts} from 'expo-font';
+import {useData, ThemeProvider, TranslationProvider} from '../hooks/';
+
+import {Platform, StatusBar} from 'react-native';
+import AppLoading from 'expo-app-loading';
+import Menu from './Menu';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
     const {userInfo, logout} = useContext(AuthContext);
+    const {isDark, theme, setTheme} = useData();
+    /* set the status bar based on isDark constant */
+    useEffect(() => {
+        Platform.OS === 'android' && StatusBar.setTranslucent(true);
+        StatusBar.setBarStyle(isDark ? 'light-content' : 'dark-content');
+        return () => {
+        StatusBar.setBarStyle('default');
+        };
+    }, [isDark]);
+    // load custom fonts
+    const [fontsLoaded] = useFonts({
+        'OpenSans-Light': theme.assets.OpenSansLight,
+        'OpenSans-Regular': theme.assets.OpenSansRegular,
+        'OpenSans-SemiBold': theme.assets.OpenSansSemiBold,
+        'OpenSans-ExtraBold': theme.assets.OpenSansExtraBold,
+        'OpenSans-Bold': theme.assets.OpenSansBold,
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+      }
+      const navigationTheme = {
+        ...DefaultTheme,
+        dark: isDark,
+        colors: {
+          ...DefaultTheme.colors,
+          border: 'rgba(0,0,0,0)',
+          text: String(theme.colors.text),
+          card: String(theme.colors.card),
+          primary: String(theme.colors.primary),
+          notification: String(theme.colors.primary),
+          background: String(theme.colors.background),
+        },
+      };
     return (
-        <NavigationContainer>
+        <TranslationProvider>
+        <ThemeProvider theme={theme} setTheme={setTheme}>
+        <NavigationContainer theme={navigationTheme}>
             <Stack.Navigator>
                 {userInfo.access_token ? (
                     <>
@@ -48,6 +89,8 @@ const Navigation = () => {
 
             </Stack.Navigator>
         </NavigationContainer>
+        </ThemeProvider>
+        </TranslationProvider>
     );
 };
 
